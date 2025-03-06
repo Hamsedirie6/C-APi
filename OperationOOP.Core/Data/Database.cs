df88ba82adc9fa9908237352d3340e871da92a29
hamsedirie6 <hamse_03@hotmail.se>
2025-03-06 15:59:16 +0100

    Skapat en lista i Database som lagrar plants

--- OperationOOP.Core/Data/Database.cs
+++ OperationOOP.Core/Data/Database.cs
@@ -5,10 +5,12 @@ namespace OperationOOP.Core.Data
     public interface IDatabase
     {
         List<Bonsai> Bonsais { get; set; }
+        List<Plant> plants {get; set;}
     }
 
     public class Database : IDatabase
     {
         public List<Bonsai> Bonsais { get; set; } = new List<Bonsai>();
+        public List<Plant> plants { get; set; } =  new List<Plant>();
     }
 }
